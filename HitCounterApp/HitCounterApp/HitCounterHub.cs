b57830ec4b941a364b8e43b605c569712ddd2f5f
MD Shohag Mia <shohaghassan14@hotmail.com>
2017-05-25 12:56:14 +0600

    Initail Commit

---
+++ HitCounterApp/HitCounterApp/HitCounterHub.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace HitCounterApp
+{
+    [HubName("hitCounterHub")]
+    public class HitCounterHub : Hub
+    {
+        static int _hitCount = 0;
+        public void RecordHit()
+        {
+            _hitCount += 1;
+            Clients.All.onRecordHit(_hitCount);
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            _hitCount -= 1;
+            Clients.All.onRecordHit(_hitCount);
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
---
+++ HitCounterApp/HitCounterApp/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(HitCounterApp.Startup))]
+
+namespace HitCounterApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
